1b2986d17c71262150754a7e4ce00e5cdd8d9549
Unknown <avaritia.95@gmail.com>
2019-02-07 14:20:07 +0100

    - Add fontAwesome and Toaster library - Divide page into two tabs: Get and Add/Update - Add input validation

--- server.js
+++ server.js
@@ -11,12 +11,13 @@
 	Schema = mongoose.Schema;
     // configuration =================
 
-    mongoose.connect('mongodb://127.0.0.1/mydb');     // connect to mongoDB database on modulus.io
+    mongoose.connect('mongodb://127.0.0.1/mydb', {useNewUrlParser: true});     // connect to mongoDB database on modulus.io
 	//Ép Mongoose sử dụng thư viện promise toàn cục
 	mongoose.Promise = global.Promise;
 	//Lấy kết nối mặc định
 	var db = mongoose.connection;
-
+	var ObjectId = mongoose.Types.ObjectId;
+	
 	//Ràng buộc kết nối với sự kiện lỗi (để lấy ra thông báo khi có lỗi)
 	db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 	
@@ -27,9 +28,6 @@
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
 
-	var projects = [{"id":"0", "text":"A"}, {"id":"1", "text":"B"}, {"id":"2", "text":"C"}, {"id":"3", "text":"D"}]
-	let topics = [];
-	let tasks = [];
 	
 	// define model =================
 	var taskSchema = new Schema(
@@ -37,6 +35,7 @@
 		taskName: { type: String, required: [true, 'Missing task name'] },
 		description: String,
 		idrequired: Boolean,
+		template: String
 		
 	});
 	var Task = mongoose.model('Task',taskSchema);
@@ -58,95 +57,211 @@
 	})
 	var Project = mongoose.model('Project',projectSchema);
 	
-	async function createProject(projectname,des){
-		var newproject = new Project({
-			projectName: projectname,
-			description: des
-		})
-		var result = await newproject.save();
-		console.log(result);
-	}
-	
-	async function createTopic(projectname,topicname,des){
-		
-		var newtopic = new Topic({
-			topicName: topicname,
-			description: des
-		})
-		Project.findOneAndUpdate({projectName: projectname}, {$push: {topics: newtopic}});
-		var result = await project.save();
-		console.log(result);
-	}
-	
-	async function createTask(topicname,taskname,des,req){
-		var newtask = new Task({
-			taskName: taskname,
-			description: des,
-			idrequired: req
-		})
-		Topic.findOneAndUpdate({topicName: topicname}, {$push: {tasks: newtask}});
-		var result1 = await topic.save();
-		var result2 = await project.save();
-		console.log(result);
-	}
 
 	// routes ======================================================================
 
     // api ---------------------------------------------------------------------
     // get all todos
-    app.get('/api/todos', function(req, res) {
+    app.get('/api/projects', function(req, res) {
 		
-		/*
+		
         // use mongoose to get all todos in the database
-        Todo.find(function(err, todos) {
+        Project.find(function(err, projs) {
 
             // if there is an error retrieving, send the error. nothing after res.send(err) will execute
             if (err)
                 res.send(err)
 
-            res.json(todos); // return all todos in JSON format
+            res.json(projs); // return all todos in JSON format
         });
-		*/
-		res.json(projects)
+		
+		//res.json(projects)
     });
 	
 	app.get('/api/topics', function(req, res) {
-		for(var i = 0; i < projects.length; i++){
-			topics.push({"id": i, "text": projects[req.query.project_id].text + projects[i].text}); 
-		}
-		res.json(topics);
+		Project.findById(req.query.projectId, function(err, proj) {
+			if (err)
+				res.send(err)
+			res.json(proj.topics);
+        });
 	});
 	
 	app.get('/api/tasks', function(req, res) {
-		for(var i = 0; i < projects.length; i++){
-			tasks.push({"id": i, "text": topics[req.query.topic_id].text + projects[i].text}); 
-		}
-		res.json(tasks);
+		Project.findById(req.query.projectId, function(err, proj) {
+			if (err)
+				res.send(err)
+			res.json(proj.topics.id(req.query.topicId).tasks);
+        });
 	});
 	
-    // create todo and send back all todos after creation
-    app.post('/api/todos', function(req, res) {
-
-        // create a todo, information comes from AJAX request from Angular
-        /*
-		Todo.create({
-            text : req.body.text,
-            done : false
-        }, function(err, todo) {
+	function findTopic(proj, name){
+		for(i in proj.topics){
+			if (proj.topics[i].topicName = name) return proj.topics[i];	
+		}
+		return null;
+	}
+	
+	function createTopic(proj, req){
+		var newtopic = new Topic({
+			topicName: req.body.topicname,
+			description: req.body.topicdes,
+			tasks: []
+		})
+	    proj.topics.push(newtopic);
+		
+		return proj.topics[proj.topics.length-1];
+	}
+	
+	function findTask(topic, name){
+		for(i in topic.tasks){
+			if (topic.tasks[i].taskName = name) return topic.tasks[i];	
+		}
+		return null;
+	}
+	
+	function createTask(topic, req){
+		var newtask = new Task({
+			taskName: req.body.taskname,
+			description: req.body.taskdes,
+			idrequired: true,
+			template: req.body.template
+		})
+		topic.tasks.push(newtask);
+		return topic.tasks[topic.tasks.length-1];
+	}
+    // create project
+    app.post('/api/add', function(req, res) {
+        // Find project, create new if not exist
+		// Find topic, create new if not exist
+		// Find task, create new if not exist
+		var update = {projectName : req.body.projname, description: req.body.projdes};
+		var query = {projectName: req.body.projname}; 
+		
+		var options = { upsert: true, new: true, setDefaultsOnInsert: true };
+		
+		Project.findOneAndUpdate(query, update, options, function(err, doc){
+			if (err) return res.send(500, { error: err });
+			switch(req.body.addType){
+				case "project": return res.send("Project is created successfully");
+				case "topic":
+					var topic = findTopic(doc,req.body.topicname);
+					if(topic != null) topic.description = req.body.topicdes;
+					else{
+						var newtopic = new Topic({
+							topicName: req.body.topicname,
+							description: req.body.topicdes,
+							tasks: []
+						})
+						doc.topics.push(newtopic);
+					}
+					doc.save(function(err,d){
+						if (err) console.log(err);
+						console.log(d);
+					})	
+					//console.log(doc);
+					return res.send("Topic is created successfully");
+				case "task":
+					var topic = findTopic(doc,req.body.topicname);
+					if(topic != null){
+						var task = findTask(topic, req.body.taskname);
+						if ( task != null ) {
+							task.description = req.body.taskdes;
+							task.template = req.body.template;
+						}
+						else{
+							createTask(topic,req);
+						}
+					}
+					else{
+						createTopic(doc,req);
+						createTask(newtopic,req);
+					}
+					return res.send("Task is created successfully");
+			}
+			
+		});
+		
+		/* Project.create({
+            projectName : req.body.projname,
+            description : req.body.projdes,
+			topics: []
+        }, function(err, proj) {
+            if (err)
+                res.send(err);
+			else{
+				res.send("Project is created successfully");
+			}
+            
+        }); */
+    });
+	
+	// create topics
+    app.post('/api/topics', function(req, res) {
+        var newtopic = new Topic({
+			topicName: req.body.title,
+			description: req.body.des,
+			tasks: []
+		})
+		Project.findOneAndUpdate({_id: req.body.projId}, {
+				$push: {topics: newtopic}
+			}, {new: true}, function (err, doc) {
+				if (err) {
+					console.log("Something wrong when updating data!");
+				}
+				res.json(doc.topics);
+			});
+    });
+	
+	// create tasks
+	app.post('/api/tasks', function(req, res) {
+        
+		console.log(req.body.projId);
+		console.log(req.body.topicId);
+		var newtask = new Task({
+			taskName: req.body.title,
+			description: req.body.des,
+			idrequired: true,
+			template: req.body.template
+		})
+		Project.findOneAndUpdate({_id: req.body.projId, 'topics._id': req.body.topicId}, {
+				$push: {'topics.$.tasks': newtask}
+			}, {new: true}, function (err, doc) {
+				console.log(doc);
+				if (err) {
+					console.log("Something wrong when updating data!");
+				}				
+				res.json(doc.topics.id(req.body.topicId).tasks);
+			});
+    });
+	
+	app.post('/api/template', function(req, res) {
+        
+		console.log(req.body.projId);
+		console.log(req.body.topicId);
+		console.log(req.body.task._id);
+		Project.findById(req.body.projId, 
+			function (err, doc) {
+				console.log(doc.topics);
+				if (err) {
+					console.log(err);
+				}				
+				res.json(doc.topics.id(req.body.topicId).tasks.id(req.body.task._id).template);
+			});
+    });
+	
+	app.post('/api/clear', function(req,res){
+		Project.remove({}, function(err) {
             if (err)
                 res.send(err);
 
             // get and return all the todos after you create another
-            Todo.find(function(err, todos) {
+            Project.find(function(err, projs) {
                 if (err)
                     res.send(err)
-                res.json(todos);
+                res.json(projs);
             });
         });
-		*/
-		res.json('post success:' + req.body.text);
-    });
-
+	});
     // delete a todo
     app.delete('/api/todos/:todo_id', function(req, res) {
         Todo.remove({
@@ -164,9 +279,10 @@
         });
     });
 	
+	app.get('/favicon.ico', (req, res) => res.status(204));
 	// application -------------------------------------------------------------
     app.get('*', function(req, res) {
-        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 	
 	// listen (start app with node server.js) ======================================
